748997e2649aedf64c2cb764586e87c1a03f3158
NeoLez <leogonzachi@gmail.com>
2025-01-06 23:53:50 -0300

    b

--- AssImp/shaders/1.model_loading.fs
+++ AssImp/shaders/1.model_loading.fs
@@ -6,6 +6,12 @@ in vec4 ViewPos;
 in vec4 WorldPos;
 
 in vec3 planeNormal;
+in vec3 planeTangent;
+in vec3 planeBitangent;
+
+in vec3 planeNormalw;
+in vec3 planeTangentw;
+in vec3 planeBitangentw;
 
 struct PointLight {
     vec3 viewPosition;
@@ -24,7 +30,17 @@ float calculateLightInfluence(vec3 fragmentViewPosition, PointLight light) {
     vec3 diff = light.viewPosition - fragmentViewPosition;
     float distance = length(diff);
 
-    return dot(normalize(diff), planeNormal)/(1 + light.attenuationCoefficient * distance * distance);
+    return clamp(dot(normalize(diff), planeNormal), 0.0, 1.0)/(1 + light.attenuationCoefficient * distance * distance);
+}
+
+float calculateSpecularLightInfluence(vec3 fragmentViewPosition, PointLight light) {
+    float specularStrength = texture(texture_specular1, TexCoords).x;
+    vec3 viewDir = - ViewPos.xyz;
+    vec3 lightDir = light.viewPosition - ViewPos.xyz;
+    vec3 reflectDir = reflect(-normalize(lightDir), planeNormal);
+    float specularStrengthAttenuation = (1 + (1/(light.attenuationCoefficient+1)) * length(viewDir) + length(lightDir));
+    float spec = pow(max(dot(normalize(viewDir), reflectDir), 0.0), 8 * specularStrengthAttenuation);
+    return specularStrength * spec * 1.5 / specularStrengthAttenuation;  
 }
 
 void main()
@@ -33,9 +49,13 @@ void main()
     for(int i = 0; i < NR_LIGHTS; i++) {
         totalLightColor += calculateLightInfluence(ViewPos.xyz, lights[i]) * lights[i].color;
     }
+    vec3 totalSpecularLight = vec3(0,0,0);
+    for(int i = 0; i < NR_LIGHTS; i++) {
+        totalSpecularLight += calculateSpecularLightInfluence(ViewPos.xyz, lights[i]) * lights[i].color;
+    }
 
-    FragColor = vec4(totalLightColor.xyz, 1.0f) * texture(texture_diffuse1, TexCoords);
-
+    FragColor = vec4(totalLightColor + totalSpecularLight.xyz, 1.0f) * texture(texture_diffuse1, TexCoords);
+    //FragColor = vec4(planeTangentw, 1.0f);
     //float a = calculateLightInfluence(ViewPos.xyz, lights[0]);
     //FragColor = vec4((calculateLightInfluence(ViewPos.xyz, lights[0]) * lights[0].color) * texture(texture_diffuse1, TexCoords).xyz, 1.0f);
 }
--- AssImp/shaders/1.model_loading.vs
+++ AssImp/shaders/1.model_loading.vs
@@ -2,12 +2,19 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
+layout (location = 2) in vec3 aTangent;
+layout (location = 2) in vec3 aBitangent;
 
 out vec2 TexCoords;
 out vec4 ViewPos;
 out vec4 WorldPos;
 out vec4 ObjectPos;
 out vec3 planeNormal;
+out vec3 planeNormalw;
+out vec3 planeTangent;
+out vec3 planeTangentw;
+out vec3 planeBitangent;
+out vec3 planeBitangentw;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -22,5 +29,12 @@ void main()
     ViewPos = view * WorldPos;
     gl_Position = projection * ViewPos;
 
-    planeNormal = mat3(transpose(inverse(view))) * mat3(transpose(inverse(model))) * aNormal; //black magic
+
+    planeNormalw = aNormal;
+    planeTangentw = aTangent;
+    planeBitangentw = aBitangent;
+    mat3 normalToView =  mat3(transpose(inverse(view * model))); //black magic
+    planeNormal = normalToView * aNormal;
+    planeTangent = normalToView * aTangent;
+    planeBitangent = normalToView * aBitangent;
 }
